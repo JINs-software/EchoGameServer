32dd87280fd14e923e7c05e46f0f1ec3b0d1eb68
JinHur <jinh2352@gmail.com>
2024-05-29 00:55:04 +0900

    1. OnStart()를 통해 EchoThread 우선 순위를 높여 처리 병목(에코 처리) 해결 시도
    2. CLanServer 송신 직렬화 버퍼 메모리 풀 활용 적용

--- EchoGameServer/EchoThread.cpp
+++ EchoGameServer/EchoThread.cpp
@@ -3,7 +3,12 @@
 
 void EchoThread::ProcessEchoMessage(SessionID sessionID, INT64 accountNo, LONGLONG sendTick)
 {
+#if defined(ALLOC_BY_TLS_MEM_POOL)
+	//JBuffer* resMsg = m_SerialBuffPoolMgr->GetTlsMemPool().AllocMem();
+	JBuffer* resMsg = GetSerialSendBuff();
+#else 
 	JBuffer* resMsg = new JBuffer(sizeof(stMSG_HDR) + sizeof(stMSG_GAME_RES_ECHO));
+#endif
 	stMSG_HDR* hdr = resMsg->DirectReserve<stMSG_HDR>();
 	hdr->code = dfPACKET_CODE;
 	hdr->len = sizeof(stMSG_GAME_RES_ECHO);
@@ -21,19 +26,27 @@ void EchoThread::ProcessEchoMessage(SessionID sessionID, INT64 accountNo, LONGLO
 	SendPacket(sessionID, resMsg);
 }
 
+void EchoThread::OnStart()
+{
+	HANDLE hThread = GetCurrentThread();
+
+	// 현재 스레드의 우선순위를 높음으로 설정
+	if (!SetThreadPriority(hThread, THREAD_PRIORITY_HIGHEST)) {
+		std::cerr << "Failed to set thread priority. Error: " << GetLastError() << "\n";
+		DebugBreak();
+	}
+
+}
+
 void EchoThread::OnRecv(SessionID sessionID, JBuffer& recvData)
 {
 	// 에코 요청 메시지 수신
 	UINT bufferOffset = 0;
 	while (recvData.GetUseSize() > bufferOffset) {
 		stMSG_HDR* hdr = (stMSG_HDR*)recvData.GetBufferPtr(bufferOffset);
-		recvData.DirectMoveDequeueOffset(sizeof(stMSG_HDR));
 		bufferOffset += sizeof(stMSG_HDR);
-
 		stMSG_GAME_REQ_ECHO* msg = (stMSG_GAME_REQ_ECHO*)recvData.GetBufferPtr(bufferOffset);
 		bufferOffset += sizeof(stMSG_GAME_REQ_ECHO);
-
-		temp_TotalRecvedMsg++;
 
 		if (!Decode(hdr->randKey, hdr->len, hdr->checkSum, (BYTE*)msg)) {
 			DebugBreak();
--- EchoGameServer/EchoThread.h
+++ EchoGameServer/EchoThread.h
@@ -1,14 +1,16 @@
 #pragma once
 #include "CLanGroupServer.h"
+
 class EchoThread : public CLanGroupThread
 {
-private:
-	UINT temp_TotalRecvedMsg = 0;
+public:
+	EchoThread(bool setTlsMemPool, size_t tlsMemPoolUnitCnt, size_t tlsMemPoolCapacity)
+		: CLanGroupThread(setTlsMemPool, tlsMemPoolUnitCnt, tlsMemPoolCapacity) {}
 private:
 	void ProcessEchoMessage(SessionID sessionID, INT64 accountNo, LONGLONG sendTick);
 
 	// 에코 스레드 초기화
-	virtual void OnStart() {}
+	virtual void OnStart();
 	// 에코 메시지 수신 처리
 	virtual void OnRecv(SessionID sessionID, JBuffer& recvData);
 	virtual void OnStop() {}
